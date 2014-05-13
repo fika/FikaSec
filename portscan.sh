84fb92be04b941eac7c758ca765d69239bab7859
Markus Hertzén <markus.hertzen@gmail.com>
2014-05-13 23:01:38 +0200

    Update portscan.sh

--- portscan.sh
+++ portscan.sh
@@ -5,12 +5,13 @@ host=$host
 firstport=$firstport
 lastport=$lastport
 
-echo "Which IP do you want to scam?"
+echo "Which IP do you want to scan?"
 read host
 echo "Which port do you want to start with?"
 read firstport
 echo "Which port do you want to end with??"
 read lastport
+clear
 
 function portscan
 
