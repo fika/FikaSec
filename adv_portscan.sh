c88da3a4a71c91fdb65637423e005931bb9cedb4
Love Dufva <love@lovedufva.se>
2014-05-14 14:10:54 +0200

    Update adv_portscan.sh
    Actually fixed the colors.

--- adv_portscan.sh
+++ adv_portscan.sh
@@ -42,17 +42,17 @@ echo ""
 echo -e "${RED_TEXT}Enter the first host to scan (ex. 1)${END}"
 read fhost
 echo ""
-echo "${RED_TEXT}Enter the last host to scan (ex. 254)${END}"
+echo -e "${RED_TEXT}Enter the last host to scan (ex. 254)${END}"
 read lhost
 echo ""
-echo "${RED_TEXT}Enter the first port to scan (ex. 1)${END}"
+echo -e "${RED_TEXT}Enter the first port to scan (ex. 1)${END}"
 read fport
 echo ""
-echo "${RED_TEXT}Enter the last port to scan (ex. 1024)${END}"
+echo -e "${RED_TEXT}Enter the last port to scan (ex. 1024)${END}"
 read lport
 echo ""
-echo "${RED_TEXT}Enter the filename where you wish to save the results.${END}"
+echo -e "${RED_TEXT}Enter the filename where you wish to save the results.${END}"
 read outfile
 echo ""
-echo "${BLUE}Starting scan. Please wait until it's finished, the results will be in $outfile${END}"
+echo -e "${BLUE}Starting scan. Please wait until it's finished, the results will be in $outfile${END}"
 for i in $(seq $fhost $lhost); do nc -v -n -z -w 1 $threeb.$i $fport-$lport >> $NOWT.txt 2>&1; done && grep succeeded $NOWT.txt > $outfile && rm $NOWT.txt
