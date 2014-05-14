d6c689143f26b5a2529f12a7561bdd8b71f9f1de
Love Dufva <love@lovedufva.se>
2014-05-14 14:20:52 +0200

    Update adv_portscan.sh
    Added cat of the output file after script finishes.

--- adv_portscan.sh
+++ adv_portscan.sh
@@ -55,4 +55,4 @@ echo -e "${INFOS}Enter the filename where you wish to save the results.${END}"
 read outfile
 echo ""
 echo -e "${BLUE}Starting scan. Please wait until it's finished, the results will be in $outfile${END}"
-for i in $(seq $fhost $lhost); do nc -v -n -z -w 1 $threeb.$i $fport-$lport >> $NOWT.txt 2>&1; done && grep succeeded $NOWT.txt > $outfile && rm $NOWT.txt
+for i in $(seq $fhost $lhost); do nc -v -n -z -w 1 $threeb.$i $fport-$lport >> $NOWT.txt 2>&1; done && grep succeeded $NOWT.txt > $outfile && rm $NOWT.txt && clear && cat $outfile
