89dd8da1ffd196345f1a6064312e8b524413dbf1
Love Dufva <love@lovedufva.se>
2014-05-14 10:02:45 +0200

    Update adv_portscan.sh

--- adv_portscan.sh
+++ adv_portscan.sh
@@ -19,4 +19,4 @@ echo "Enter the filename where you wish to save the results."
 read outfile
 echo ""
 echo "Starting scan. Please wait until it's finished, the results will be in $outfile"
-for i in $(seq $fhost $lhost); do nc -v -n -z -w 1 $3B.$i $fport-$lport >> scan.txt 2>&1; done && grep succeeded scan.txt > scanresult.txt && rm scan.txt
+for i in $(seq $fhost $lhost); do nc -v -n -z -w 1 $threeb.$i $fport-$lport >> scan.txt 2>&1; done && grep succeeded scan.txt > scanresult.txt && rm scan.txt
