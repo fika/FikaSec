abfb7fcafcf87335ba089d3ca759308d43fb0cbe
zergsaint <ljm.borjesson@gmail.com>
2014-05-15 08:39:51 +0200

    Update adv_portscan.sh

--- adv_portscan.sh
+++ adv_portscan.sh
@@ -56,3 +56,12 @@ read outfile
 echo ""
 echo -e "${BLUE}Starting scan. Please wait until it's finished, the results will be in $outfile${END}"
 for i in $(seq $fhost $lhost); do nc -v -n -z -w 1 $threeb.$i $fport-$lport >> $NOWT.txt 2>&1; done && grep succeeded $NOWT.txt > $outfile && rm $NOWT.txt && clear && cat $outfile
+read -r -p "Do you want to go back to the Main menu? [y/N] " response
+case $response in
+[yY][eE][sS]|[yY])
+        bash FikaSec
+;;
+    *)
+        :
+;;
+esac
