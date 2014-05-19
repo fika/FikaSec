38014857ddcc55bd23b5d73af04cede8ba82b151
volten <viktor.voltaire@gmail.com>
2014-05-19 16:32:13 +0200

    Create reversetunnel.sh

---
+++ reversetunnel.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo -e "Enter the host you want to tunnel through"
+read hostip
+echo -e "Enter the port you want to forward with (example 2210)"
+read forport
+echo -e "Enter localhost or the ip of another host, like a printer"
+read localip
+echo -e "Enter the port of the service you will tunnel (example 22 for ssh)"
+read localport
+echo -e "Enter the username for the gateway"
+read user
+
+ssh -N -f -R $hostip:$forport:$localip:$localport $user@$hostip
+echo "Make sure that the gateway has GatewayPorts clientspecified in sshd_config"
